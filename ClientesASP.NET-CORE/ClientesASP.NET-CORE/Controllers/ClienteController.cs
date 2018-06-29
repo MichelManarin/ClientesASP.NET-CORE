6269823e04cef3544f6ec21f5efcab3e95985b7e
Michel Manarin <MichelManarin@gmail.com>
2018-06-28 23:30:08 -0300

    Passo 1 - Iniciando a estrutura do site  - Construção das views - Aplicando alguns elementos Boostrap para estilização das views.

---
+++ ClientesASP.NET-CORE/ClientesASP.NET-CORE/Controllers/ClienteController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clientes.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Menu()
+        {
+            return View();
+        }
+
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        public IActionResult Relatorio()
+        {
+            return View();
+        }
+    }
+}
