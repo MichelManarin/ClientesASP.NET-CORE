9569f047efccb4c7c4de30808cf4ffafa66b3154
Michel Manarin <MichelManarin@gmail.com>
2018-06-29 00:08:22 -0300

    Passo 2 - Adição de action link e outras configurações

--- ClientesASP.NET-CORE/ClientesASP.NET-CORE/Startup.cs
+++ ClientesASP.NET-CORE/ClientesASP.NET-CORE/Startup.cs
@@ -53,7 +53,7 @@ namespace Pessoas
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Cliente}/{action=Menu}/{id?}");
             });
         }
     }
